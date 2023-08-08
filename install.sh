5a95f91dec234d1e7138d9d65b6d40fbb2279407
Shannon Graybrook <552756+realslacker@users.noreply.github.com>
2023-08-08 16:12:32 -0500

    functional install for 22.04

--- install.sh
+++ install.sh
@@ -13,69 +13,73 @@ echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 # Upgrade system
 apt-get update && apt-get -yq dist-upgrade
 
+# install git if needed
+apt -yq install git
+
+# clone this repository
+git clone https://github.com/realslacker/rom-o-matic.git /opt/rom-o-matic
+git -C /opt/rom-o-matic submodule init
+git -C /opt/rom-o-matic submodule update
+rm -rf /var/www/html
+ln -s /opt/rom-o-matic/public /var/www/html
+#ln -s /opt/rom-o-matic/scripts/parseheaders.pl /opt/rom-o-matic/ipxe/src/util/
+chown -R www-data:www-data /opt/rom-o-matic
+git config --global --add safe.directory /opt/rom-o-matic
+
 # Install basic compilation tools and dev libraries
-apt install -yq build-essential \
-                    iasl lzma-dev mtools perl python3 \
-                    subversion uuid-dev liblzma-dev mtools
-#apt-get -y install make gcc zlib1g-dev libc6-dev libssl-dev libstdc++6-4.7-dev libc-dev-bin liblzma-dev
+apt -yq install \
+    build-essential \
+    iasl lzma-dev mtools perl python3 \
+    subversion uuid-dev liblzma-dev mtools
 
 # Install CGI Perl dependencies
-apt-get -yq install liburi-perl \
-                      libfcgi-perl \
-                      libconfig-inifiles-perl \
-                      libipc-system-simple-perl \
-                      libsub-override-perl \
-                      libcgi-pm-perl
+apt-get -yq install \
+    liburi-perl \
+    libfcgi-perl \
+    libconfig-inifiles-perl \
+    libipc-system-simple-perl \
+    libsub-override-perl \
+    libcgi-pm-perl
 
 #  Prepare iPXE directory
-mkdir -p /var/cache/ipxe-build /var/run/ipxe-build /var/tmp/ipxe-build
-rm -rf /var/cache/ipxe-build/* /var/run/ipxe-build/* /var/tmp/ipxe-build/*
-
-# Install Git tools
-apt-get -y install git
+mkdir -p \
+    /var/cache/ipxe-build \
+    /var/run/ipxe-build \
+    /var/tmp/ipxe-build
+rm -rf \
+    /var/cache/ipxe-build/* \
+    /var/run/ipxe-build/* \
+    /var/tmp/ipxe-build/*
 
 # Prepare the git iPXE repository
-cd /var/tmp/ && rm -rf ipxe && git clone https://github.com/ipxe/ipxe/
 touch /var/run/ipxe-build/ipxe-build-cache.lock
-chown -R www-data:www-data /var/run/ipxe-build/ipxe-build-cache.lock \
-                           /var/cache/ipxe-build \
-                           /var/run/ipxe-build \
-                           /var/tmp/ipxe-build /var/tmp/ipxe
+chown -R www-data:www-data \
+    /var/run/ipxe-build/ipxe-build-cache.lock \
+    /var/cache/ipxe-build \
+    /var/run/ipxe-build \
+    /var/tmp/ipxe-build \
+    /opt/ipxe
 
 # Install Apache with fast CGI and PHP module
-#apt-get -y install libapache2-mod-fcgid libapache2-mod-php5 && a2enmod fcgid php5
-#apt-get -yq install libapache2-mod-fcgid libapache2-mod-php && a2enmod fcgid php7.4
-apt-get -yq install libapache2-mod-fcgid libapache2-mod-php && a2enmod fcgid php
+apt-get -yq install \
+    libapache2-mod-fcgid \
+    libapache2-mod-php
+a2enmod fcgid php8.1
 
 # Install JSON library Perl
-apt-get -yq install libjson-perl libjson-any-perl libjson-xs-perl
+apt-get -yq install \
+    libjson-perl \
+    libjson-any-perl \
+    libjson-xs-perl
 
 # Install extra packages to allow to build ISO and EFI binary
-apt-get -yq install binutils-dev genisoimage syslinux isolinux
+apt-get -yq install \
+    binutils-dev \
+    genisoimage \
+    syslinux \
+    isolinux
 
-# Prepare the git buildweb repository
-mkdir -p /var/www && cd /var/www && git clone https://github.com/xbgmsharp/ipxe-buildweb.git
-cd /var/www/ipxe-buildweb
-cp parseheaders.pl /var/tmp/ipxe/src/util/
-
-# List required binaries
-which perl make git syslinux
-
-# message
-echo -e "\nYou can now configure your webserver Apache.\nImportant directories:\n\t/var/cache/ipxe-build /var/run/ipxe-build /var/tmp/ipxe-build /var/www/ipxe-buildweb"
-
-# setting up apache2
-cat << EOF > /etc/apache2/sites-enabled/000-default.conf
-<VirtualHost *:80>
-	ServerAdmin webmaster@localhost
-	DocumentRoot /var/www/ipxe-buildweb
-
-	ErrorLog ${APACHE_LOG_DIR}/error.log
-	CustomLog ${APACHE_LOG_DIR}/access.log combined
-
-</VirtualHost>
-EOF
-
+# configure fast-cgi
 cat << EOF > /etc/apache2/mods-enabled/fcgid.conf
 <IfModule mod_fcgid.c>
     FcgidConnectTimeout 120
@@ -93,11 +97,4 @@ cat << EOF > /etc/apache2/mods-enabled/fcgid.conf
 </IfModule>
 EOF
 
-DIRECTORIES="/var/tmp/ipxe-build /var/tmp/ipxe"
-for DIRECTORY in ${DIRECTORIES}
-do
- if [[ ! -d "${DIRECTORY}" ]]; then
-     echo "Error, missing directory ${DIRECTORY}"
-     exit 1
- fi
-done
+systemctl reload apache2
