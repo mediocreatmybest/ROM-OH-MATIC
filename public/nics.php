982ce12c535632b2116ea2f33ce8677f1c088b88
Shannon Graybrook <552756+realslacker@users.noreply.github.com>
2023-08-08 09:40:00 -0500

    update nics.php

--- public/nics.php
+++ public/nics.php
@@ -10,16 +10,16 @@
 #------------------------------------------------------------------------
 */
 
-$cache_file = "/tmp/ipxelistnics";
+$cache_path = dirname(__FILE__) + "/cache";
+$cache_file = "$cache_path/nics.json";
 $cache_life = '3600'; //caching time, in seconds, 1h
 
 $filemtime = @filemtime($cache_file);  // returns FALSE if file does not exist
 if (!$filemtime or (time() - $filemtime >= $cache_life))
 {
-	$outpout = exec("rm -f /tmp/ipxelistnics && cd /var/tmp/ipxe/src/ && /var/tmp/ipxe/src/util/niclist.pl --format json --columns ipxe_name,device_id,vendor_id 1>/tmp/ipxelistnics");
-	readfile("/tmp/ipxelistnics");
-} else {
-	readfile("/tmp/ipxelistnics");
+	$command = '/opt/ipxe/src/util/niclist.pl --format json --output /var/www/html/cache/nics.json 2> /dev/null';
+	exec($command, $output, $result);
 }
+readfile($cache_file);
 
 ?>
