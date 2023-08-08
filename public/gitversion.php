fdcad3a7f61615b8ddc3260f73d0e8fa1e58000e
Shannon Graybrook <552756+realslacker@users.noreply.github.com>
2023-08-08 10:20:27 -0500

    update gitversion.php

--- public/gitversion.php
+++ public/gitversion.php
@@ -10,6 +10,14 @@
 #------------------------------------------------------------------------
 */
 
+$command = "git config safe.directory | grep /opt/rom-o-matic/ipxe";
+exec($command, $output, $result);
+if ($result <> 0)
+{
+    $command = "git config --global --add safe.directory /opt/rom-o-matic/ipxe";
+    exec($command, $output, $result);
+}
+
 $command = "git -C /opt/rom-o-matic/ipxe rev-list --all --max-count=30 --abbrev-commit --abbrev=1";
 exec($command, $output, $result);
 echo json_encode($output);
